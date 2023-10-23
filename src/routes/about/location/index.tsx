299601449ea9cbea749523ed69f031d6079b5f68
duongpham17 <duongpham17@icloud.com>
2023-10-23 22:13:36 +0100

    map

--- src/routes/about/location/index.tsx
+++ src/routes/about/location/index.tsx
@@ -1,55 +1,45 @@
-// import React, { useEffect, useRef } from 'react';
-// import { Loader } from '@googlemaps/js-api-loader';
+import React, { useEffect, useRef } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 
-// const GoogleMap = () => {
-//   const mapContainer = useRef(null);
+const GoogleMap = () => {
+  const mapContainer = useRef(null);
 
-//   useEffect(() => {
-//     const api = process.env.NEXT_PUBLIC_API_GOOGLE_MAP_API_KEY as string;
+  useEffect(() => {
+    const api = process.env.NEXT_PUBLIC_API_GOOGLE_MAP_API_KEY as string;
 
-//     const initializeMap = async () => {
-//       const loader = new Loader({
-//         apiKey: api,
-//         version: 'weekly',
-//       });
+    const initializeMap = async () => {
+      const loader = new Loader({
+        apiKey: api,
+        version: 'weekly',
+      });
 
-//       try {
-//         const googleApi: any = await loader.load();
+      try {
+        const googleApi: any = await loader.load();
         
-//         const map = new googleApi.maps.Map(mapContainer.current as any, {
-//           center: { lat: 51.50167594341143, lng: -0.22241806441828063 },
-//           zoom: 12,
-//         });
+        const map = new googleApi.maps.Map(mapContainer.current as any, {
+          center: { lat: 51.50167594341143, lng: -0.22241806441828063 },
+          zoom: 12,
+        });
 
-//         new googleApi.maps.Marker({
-//           position: { lat: 51.50167594341143, lng: -0.22241806441828063 },
-//           map: map,
-//           title: 'The Nailist',
-//         });
-//       } catch (error) {
-//         console.error('Failed to load Google Maps:', error);
-//       }
-//     };
+        new googleApi.maps.Marker({
+          position: { lat: 51.50167594341143, lng: -0.22241806441828063 },
+          map: map,
+          title: 'The Nailist',
+        });
+      } catch (error) {
+        console.error('Failed to load Google Maps:', error);
+      }
+    };
 
-//     initializeMap();
+    initializeMap();
 
-//   }, []);
+  }, []);
 
-//   return (
-//     <div id="location">
-//       <div ref={mapContainer} style={{ height: '500px', width: '100%' }} />
-//     </div>
-//   );
-// };
-
-// export default GoogleMap;
-
-import React from 'react'
-
-const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div id="location">
+      <div ref={mapContainer} style={{ height: '500px', width: '100%' }} />
+    </div>
+  );
+};
 
-export default index
+export default GoogleMap;
