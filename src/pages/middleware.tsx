d4c79597a130390938f36bd2ea5f7b4633d50119
duongpham17 <duongpham17@icloud.com>
2023-10-26 20:30:26 +0100

    bugs

--- src/pages/middleware.tsx
+++ src/pages/middleware.tsx
@@ -1,30 +1,30 @@
 // middleware.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import jwt from 'jsonwebtoken';
-import UserModel from '@database/models/users';
+// import jwt from 'jsonwebtoken';
+// import UserModel from '@database/models/users';
 
 export default function myMiddleware(handler: (req: NextApiRequest, res: NextApiResponse) => Promise<void>) {
   return async (req: NextApiRequest, res: NextApiResponse) => {
 
-    if(req.method === "GET") return await handler(req, res);
+    // if(req.method === "GET") return await handler(req, res);
 
-    let token;
+    // let token;
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-      token = req.headers.authorization.split(' ')[1];
-    };
+    // if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+    //   token = req.headers.authorization.split(' ')[1];
+    // };
 
-    if(!token) return res.status(401).json({ message: 'Unauthorized' });;
+    // if(!token) return res.status(401).json({ message: 'Unauthorized' });;
 
-    const jwt_secret:any = process.env.JWT_SECRET;
+    // const jwt_secret:any = process.env.JWT_SECRET;
 
-    const decodedId:any = jwt.verify(token, jwt_secret);
+    // const decodedId:any = jwt.verify(token, jwt_secret);
 
-    const existingUser = await UserModel.findById(decodedId.id);
+    // const existingUser = await UserModel.findById(decodedId.id);
 
-    if(!existingUser) return res.status(401).json({ message: 'Unauthorized' });
+    // if(!existingUser) return res.status(401).json({ message: 'Unauthorized' });
 
-    if(existingUser.role === "user") return res.status(401).json({ message: 'Unauthorized' }); 
+    // if(existingUser.role === "user") return res.status(401).json({ message: 'Unauthorized' }); 
 
     await handler(req, res);
   };
