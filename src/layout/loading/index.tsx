a06e9654e4ff74159682746540753d7003d7359b
duongpham17 <duongpham17@icloud.com>
2023-10-27 01:18:33 +0100

    first commit

--- src/layout/loading/index.tsx
+++ src/layout/loading/index.tsx
@@ -3,16 +3,20 @@ import React from 'react';
 
 const Loading = () => {
 
-    const set = process.env.NODE_ENV === "production";
+    // const set = process.env.NODE_ENV === "production";
 
-    return ( set ? 
-        <div className={styles.container}>
-            <div className={styles.logo}>
-                <span>THE NAILIST</span>
-                <span>NAILS - BROWS - BEAUTY</span>
-            </div>
-        </div>
-        :
+    // return ( set ? 
+    //     <div className={styles.container}>
+    //         <div className={styles.logo}>
+    //             <span>THE NAILIST</span>
+    //             <span>NAILS - BROWS - BEAUTY</span>
+    //         </div>
+    //     </div>
+    //     :
+    //     null
+    // )
+
+    return (
         null
     )
 }
