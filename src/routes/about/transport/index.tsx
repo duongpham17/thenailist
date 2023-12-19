3c10ce5467c11fc4bb5d21c7b22f6498a549e9a2
duongpham17 <duongpham17@icloud.com>
2023-12-19 00:32:40 +0000

    trasnport

--- src/routes/about/transport/index.tsx
+++ src/routes/about/transport/index.tsx
@@ -1,15 +1,11 @@
 import styles from './Transport.module.scss';
 import React from 'react';
 import {data} from './data';
-import useWindow from '@hooks/useWindow';
-import Swiper from '@components/swiper';
 
 const Transport = () => {
 
-  const {width} = useWindow()
-
-  return ( width >= 1000 ?
-    <div className={styles.large} id="location">
+  return (
+    <div className={styles.container} id="location">
         <h1>GETTING HERE</h1>
         <div className={styles.transport}>
           {data.map(el =>
@@ -19,18 +15,6 @@ const Transport = () => {
             </div>    
           )}
         </div>
-    </div>
-    :
-    <div className={styles.small} id="location">
-      <h1>GETTING HERE</h1>
-      <Swiper data={data} slidersPerView={1} arrows>
-        {(el =>
-          <div className={styles.element} key={el.id}>
-            <h2>{el.name}</h2>
-            <p>{el.description}</p>
-          </div>    
-        )}
-      </Swiper>
     </div>
   )
 }
